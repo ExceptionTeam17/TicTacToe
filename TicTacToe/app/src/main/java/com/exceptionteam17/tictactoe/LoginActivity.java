b6df6a815271c8ecc190ca4b8a87e090ec763725
AleksandarMironov <aleksandar.n.mironov@gmail.com>
2017-12-17 22:37:59 +0200

    :)

---
+++ TicTacToe/app/src/main/java/com/exceptionteam17/tictactoe/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.exceptionteam17.tictactoe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        setOnLoginListener();
+    }
+
+    private void setOnLoginListener(){
+        findViewById(R.id.login_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.putExtra("user", "nqma sega");
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ TicTacToe/app/src/main/java/com/exceptionteam17/tictactoe/Preferences.java
@@ -0,0 +1,14 @@
+package com.exceptionteam17.tictactoe;
+
+import android.content.Context;
+
+/**
+ * Created by Mironovi on 17.12.2017 г..
+ */
+
+public abstract class Preferences {
+
+    public static String getFromPref(Context context, String key){
+        return "";
+    }
+}
