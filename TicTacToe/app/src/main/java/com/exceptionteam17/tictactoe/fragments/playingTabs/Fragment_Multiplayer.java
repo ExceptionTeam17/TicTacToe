b707a2af1c4964445754f943d8f75748f30b3c32
Ivan <roni10b@abv.bg>
2017-12-30 19:18:05 +0200

    bluetooth tests

--- TicTacToe/app/src/main/java/com/exceptionteam17/tictactoe/fragments/playingTabs/Fragment_Multiplayer.java
+++ TicTacToe/app/src/main/java/com/exceptionteam17/tictactoe/fragments/playingTabs/Fragment_Multiplayer.java
@@ -2,21 +2,31 @@ package com.exceptionteam17.tictactoe.fragments.playingTabs;
 
 import android.Manifest;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.exceptionteam17.tictactoe.R;
 import com.exceptionteam17.tictactoe.model.database.DatabaseHelper;
 import com.exceptionteam17.tictactoe.model.utils.Preferences;
+
+import java.util.Set;
 
 public class Fragment_Multiplayer extends Fragment{
 
@@ -25,12 +35,13 @@ public class Fragment_Multiplayer extends Fragment{
     private TextView win, draw, lost, usernameView;
     private DatabaseHelper db;
     private static final int REQUEST_PERMISSION = 1;
+    private Button start;
+    private BluetoothAdapter bluetoothAdapter;
     private static String[] PERMISSIONS= {
             Manifest.permission.BLUETOOTH,
             Manifest.permission.BLUETOOTH_ADMIN,
             //Manifest.permission.BLUETOOTH_PRIVILEGED api 19+
     };
-    private Button start;
 
     @Nullable
     @Override
@@ -51,6 +62,7 @@ public class Fragment_Multiplayer extends Fragment{
         lost = view.findViewById(R.id.toolbar_lost);
         draw = view.findViewById(R.id.toolbar_draw);
         usernameView.setText(username);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         setMulti();
     }
 
@@ -77,7 +89,16 @@ public class Fragment_Multiplayer extends Fragment{
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadFragment(new Fragment_Gameplay());
+//                Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//                startActivity(turnOn);
+                if (bluetoothAdapter == null) {
+                    Toast.makeText(view.getContext(), "NO BLUETOOTH", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (!bluetoothAdapter.isEnabled()) {
+                    bluetoothAdapter.enable();
+//                    loadFragment(new Fragment_Gameplay());
+                }
             }
         });
     }
