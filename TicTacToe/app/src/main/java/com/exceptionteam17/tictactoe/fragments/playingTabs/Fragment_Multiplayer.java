d0d2abea131c6f2421162b6c33d520be94427414
Mironov <aleksandar.n.mironov@gmail.com>
2017-12-30 18:04:00 +0200

    permission added

--- TicTacToe/app/src/main/java/com/exceptionteam17/tictactoe/fragments/playingTabs/Fragment_Multiplayer.java
+++ TicTacToe/app/src/main/java/com/exceptionteam17/tictactoe/fragments/playingTabs/Fragment_Multiplayer.java
@@ -1,11 +1,17 @@
 package com.exceptionteam17.tictactoe.fragments.playingTabs;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.exceptionteam17.tictactoe.R;
@@ -18,17 +24,27 @@ public class Fragment_Multiplayer extends Fragment{
     private String username;
     private TextView win, draw, lost, usernameView;
     private DatabaseHelper db;
+    private static final int REQUEST_PERMISSION = 1;
+    private static String[] PERMISSIONS= {
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            //Manifest.permission.BLUETOOTH_PRIVILEGED api 19+
+    };
+    private Button start;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_multiplayer, container, false);
         initialize();
+        verifyPermissions(((Activity)view.getContext()));
+        startGame();
         return view;
     }
 
     private void initialize() {
         db = DatabaseHelper.getInstance(view.getContext());
+        start = view.findViewById(R.id.start_gameplay_multi);
         username = Preferences.getStringFromPreferences(view.getContext(),"user");
         usernameView = view.findViewById(R.id.toolbar_username);
         win = view.findViewById(R.id.toolbar_win);
@@ -42,5 +58,33 @@ public class Fragment_Multiplayer extends Fragment{
         win.setText("Wins: " + db.getUserWinsMulti(username));
         draw.setText("Draws: " + db.getUserDrawsMulti(username));
         lost.setText("Loses: " + db.getUserLosesMulti(username));
+    }
+
+    public static void verifyPermissions(Activity activity) {
+        int btAdmin = ActivityCompat.checkSelfPermission(activity, Manifest.permission.BLUETOOTH_ADMIN);
+        int bt = ActivityCompat.checkSelfPermission(activity, Manifest.permission.BLUETOOTH);
+
+        if (bt != PackageManager.PERMISSION_GRANTED || btAdmin != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS,
+                    REQUEST_PERMISSION
+            );
+        }
+    }
+
+    private void startGame() {
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new Fragment_Gameplay());
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.home_main, fragment);
+        ft.commit();
     }
 }
