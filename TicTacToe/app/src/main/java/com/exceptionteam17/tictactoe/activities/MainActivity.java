725b4b0be921b1b4782103b4e1cf8ac2ba8edf63
Mironov <aleksandar.n.mironov@gmail.com>
2018-03-29 21:31:38 +0300

    test add added

--- TicTacToe/app/src/main/java/com/exceptionteam17/tictactoe/activities/MainActivity.java
+++ TicTacToe/app/src/main/java/com/exceptionteam17/tictactoe/activities/MainActivity.java
@@ -4,12 +4,19 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
 
 import com.exceptionteam17.tictactoe.R;
 import com.exceptionteam17.tictactoe.fragments.Fragment_Home;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity{
+
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +26,14 @@ public class MainActivity extends AppCompatActivity{
         if(getWindow() != null) {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         }
-        InitElements();
         loadFragment(new Fragment_Home());
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713"); //TODO test id
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
     private void loadFragment(Fragment fragment) {
@@ -35,7 +48,30 @@ public class MainActivity extends AppCompatActivity{
         }
     }
 
-    private void InitElements(){
 
+    @Override
+    protected void onPause() {
+        mInterstitialAd = new InterstitialAd(this.getApplicationContext());
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712"); //TODO test id
+        AdRequest adRequest = new AdRequest.Builder().addTestDevice("61D5A593BF290904C3894DE2CF09847D").build();  //TODO test device
+        mInterstitialAd.loadAd(adRequest);
+        mInterstitialAd.setAdListener(new AdListener() {
+            public void onAdLoaded() {
+                showInterstitial();
+            }
+
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                MainActivity.this.finish();
+            }
+        });
+        super.onPause();
+    }
+
+    private void showInterstitial() {
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        }
     }
 }
